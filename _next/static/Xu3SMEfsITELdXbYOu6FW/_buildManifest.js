e23236286dde1ca15b1532fd139766c6bbb29113
Hongyi Ding <dinghy@seas.upenn.edu>
2025-11-09 02:23:47 -0500

    Added sidebar

---
+++ _next/static/Xu3SMEfsITELdXbYOu6FW/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Bird-Collision-Map-Sharkbyte//_next/:path+",
+        "destination": "/Bird-Collision-Map-Sharkbyte/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
